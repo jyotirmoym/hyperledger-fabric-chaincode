79faead0b3698abce9b3a1566d9f959cc85d2990
Jyotirmoy Mukherjee <64358983+jyotirmoy-mukherjee@users.noreply.github.com>
2021-06-24 23:41:32 +0100

    Updated

--- src/main/java/com/explorer/commodity/ledger/model/Commodity.java
+++ src/main/java/com/explorer/commodity/ledger/model/Commodity.java
@@ -3,6 +3,10 @@ package com.explorer.commodity.ledger.model;
 import org.hyperledger.fabric.contract.annotation.DataType;
 import org.hyperledger.fabric.contract.annotation.Property;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.Data;
 
 @DataType
@@ -14,5 +18,16 @@ public class Commodity {
 	
 	@Property
 	private Long count;
+	
+	public String toJSONString() throws JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		return mapper.writeValueAsString(this);
+    }
+
+    public static Commodity fromJSONString(String json) throws JsonMappingException, JsonProcessingException {
+    	ObjectMapper mapper = new ObjectMapper();
+    	Commodity deal = mapper.readValue(json, Commodity.class);
+        return deal;
+    }
 
 }
